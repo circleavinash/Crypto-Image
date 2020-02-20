56e47b2d6f869561d94fafcdd7376f70ed352613
circleavinash <51701317+circleavinash@users.noreply.github.com>
2020-02-20 14:23:10 +0530

    Update coding.py

--- coding.py
+++ coding.py
@@ -1,6 +1,9 @@
 import numpy as np
 import cv2
 import sys
+from matplotlib import pyplot as plt
+import textwrap
+import argparse
 
 def split(img):
 	l = []
@@ -18,17 +21,22 @@ def merge(l):
 
 
 def get_text(img, text):
+	factor = 1/20
+	text = textwrap.wrap(text, width=len(img[0])*factor)
 	sh = img.shape
 	txt = np.ones(shape=sh)
-	# cv2.imshow("blank", txt)
-
+	
 	fontface = cv2.FONT_HERSHEY_SIMPLEX
-	fontscale = 1
-	thickness = 2
+	fontscale = 0.7
+	thickness = 2	
 	color = (0, 0, 0)
-	orig = (10, 100)
+	x = 10
+	y = 20
+	orig = (x, y)
 	linetype = cv2.LINE_AA
-	txt1 = cv2.putText(txt, text, orig, fontface, fontscale, color, thickness, linetype)
+	for i in text:
+		txt1 = cv2.putText(txt, i, orig, fontface, fontscale, color, thickness, linetype)
+		orig = (orig[0], orig[1]+30)
 	txt2 = txt1 // 255
 	txt2 = txt.astype("uint8")
 	return txt2
@@ -61,56 +69,46 @@ def decoder(image):
 		print("Invalid image")
 
 
+
 if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-a", "--action", required=True)
+	parser.add_argument("-i", "--image", required=True)
+	parser.add_argument("-t", "--text", nargs="+")
+	args = parser.parse_args()
+
 	inp = sys.argv
 	img = None
 	text = None
-	if len(inp) == 4:
-		if inp[1] == "encode":
-			try:
-				filename = inp[2]
-				img = cv2.imread(filename)
-				text = inp[3]
-			except FileNotFoundError:
-				print("File does not exist.")
-				exit(0)
-			new_img = encoder(img, text)
-			tst = decoder(new_img)
-			cv2.imshow("in enc", tst)
-			cv2.waitKey(0)
-			cv2.destroyAllWindows()
-	
-			tmp = filename.split(".")
-			new_filename = tmp[0]+"_encoded.png"	# This is important. Doesn't work if image saved as jpg
-			cv2.imwrite(new_filename, new_img)
+	if args.action == "encode":
+		try:
+			filename = args.image
+			img = cv2.imread(filename)
+			text = ' '.join(args.text)
+		except FileNotFoundError:
+			print("File does not exist.")
+			exit(0)
+		new_img = encoder(img, text)
+		tmp = filename.split(".")
+		new_filename = tmp[0]+"_encoded.png"	# This is important. Doesn't work if image saved as jpg
+		cv2.imwrite(new_filename, new_img)
+		print("Successfully encoded")
+		
 
-			print(new_filename)
-			tst2 = cv2.imread(new_filename)
-			tst2 = decoder(tst2)
-			cv2.imshow("in enc", tst2)
-			cv2.waitKey(0)
-			cv2.destroyAllWindows()
-	
-			print("Successfully encoded")
-		else:
-			print("Invalid command")
-
-	elif len(inp) == 3:
-		if inp[1] == "decode":
-			try:
-				filename = inp[2]
-				img = cv2.imread(filename)
-			except FileNotFoundError:
-				print("File does not exist.")
-				exit(0)
-			new_img = decoder(img)
-			cv2.imshow("decoded", new_img)
-			cv2.waitKey(0)
-			cv2.destroyAllWindows()
-			tmp = filename.split(".")
-			new_filename = tmp[0]+"_decoded."+tmp[1]
-			cv2.imwrite(new_filename, new_img)
-			print("Successfully decoded")
-
+	elif args.action == "decode":
+		try:
+			filename = args.image
+			img = cv2.imread(filename)
+		except FileNotFoundError:
+			print("File does not exist.")
+			exit(0)
+		new_img = decoder(img)
+		cv2.imshow("decoded", new_img)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
+		tmp = filename.split(".")
+		new_filename = tmp[0]+"_decoded."+tmp[1]
+		cv2.imwrite(new_filename, new_img)
+		print("Successfully decoded")
 
 
